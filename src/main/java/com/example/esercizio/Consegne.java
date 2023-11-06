f9dc8f7413acbf790dc1cd49f7fed8f068d5981e
unknown <mark881270@gmail.com>
2023-11-06 14:50:33 +0100

    Second Commit

--- src/main/java/com/example/esercizio/Consegne.java
+++ src/main/java/com/example/esercizio/Consegne.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EsercizioApplication {
+public class Consegne {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EsercizioApplication.class, args);
+		SpringApplication.run(Consegne.class, args);
 	}
 
 }
